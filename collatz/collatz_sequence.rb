d6dc88404f17bd78ba87c1f0de39f507b0c3edd0
masoodgit <mohamed.alam78@gmail.com>
2016-01-02 00:13:14 +0000

    find the longest collatz sequence

--- collatz/collatz_sequence.rb
+++ collatz/collatz_sequence.rb
@@ -10,10 +10,37 @@ def collatz_sequence(num)
       items << num
     end
   end
-  puts items.inspect
-  puts "sequence length : #{items.length}"
+  #puts items.inspect
+  #puts "sequence length : #{items.length}"
+  return items.length
 end
 
-puts collatz_sequence(4)
-puts collatz_sequence(7)
-puts collatz_sequence(20)
+def longest_collatz_sequence
+  limit = 1000000
+  longestSequence = 0
+  longestSequenceFor = 0
+  i = 1
+  while i <= limit
+    sequenceLength = collatz_sequence(i)
+    if sequenceLength > longestSequence
+      longestSequence = sequenceLength
+      longestSequenceFor = i
+    end
+    i += 1
+  end
+  puts "LongestSequence of #{longestSequence} found for #{longestSequenceFor}"
+end
+
+puts '====================================='
+puts 'starting..............................'
+longest_collatz_sequence()
+puts '====================================='
+
+
+
+
+
+#puts collatz_sequence(4)
+#puts collatz_sequence(7)
+#puts collatz_sequence(20)
+#puts collatz_sequence(5600112)
